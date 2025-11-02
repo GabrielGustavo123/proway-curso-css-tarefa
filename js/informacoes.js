8096453b6727bc93e85714fe7aff1114df2c1971
GabrielGustavo123 <mecianogabriel11@gmail.com>
2025-11-02 15:01:31 -0300

    jogos finalizados

--- js/informacoes.js
+++ js/informacoes.js
@@ -1,9 +1,10 @@
 let InputNome = document.getElementById("usuario");
-let nome = InputNome.value;   
+   
 
 
 function salvar(){
     let nomeString = JSON.stringify(nome);
     localStorage.setItem("nomeUsuario", nomeString);
+    let nome = InputNome.value;
     window.location.href = "perfil.html";
 }
